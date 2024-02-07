8104d87f94d41c97d81ab284006a46edfedd3434
semirDedic <semir.dedic04@gmail.com>
2024-02-07 01:52:32 +0100

    Added media upload for posts
    - Added new package "spatie/laravel-medialibrary"
    - Published config for "spatie/laravel-medialibrary"
    - Published migration for "spatie/laravel-medialibrary"
    - Added new trait for Media because we need to crop images for thumbnails
    - Added "media" with autoloading relations for Post model
    - Edited getter for translation attributes in livewire request validation for Posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Traits\HasMedia;
 use App\Models\Traits\Taggable;
 use App\Models\Traits\Translatable;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
@@ -9,10 +10,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia as HasMediaContract;
 
-class Post extends Model implements TranslatableContract
+class Post extends Model implements HasMediaContract, TranslatableContract
 {
-    use HasFactory, Translatable, Taggable, SoftDeletes;
+    use HasMedia, HasFactory, Translatable, Taggable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable..
@@ -33,11 +35,21 @@ class Post extends Model implements TranslatableContract
      *
      * @var array
      */
-    protected $with = ['translations'];
+    protected $with = ['media', 'translations'];
 
     protected $casts = [
         'active' => 'boolean',
     ];
+
+    /**
+     * Image attribute.
+     *
+     * @return \Lit\Crud\Models\Media
+     */
+    public function getImageAttribute()
+    {
+        return $this->getMedia('image')->first();
+    }
 
     /**
      * Get the user that owns the post.
